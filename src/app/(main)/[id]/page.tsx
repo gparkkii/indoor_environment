e9df590983b684d20e183d512e3eeb53588b4635
gparkkii <jyp933@gmail.com>
2024-09-23 01:43:29 +0900

    fix build error

--- src/app/(main)/[id]/page.tsx
+++ src/app/(main)/[id]/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import styles from '../page.module.css';
 import { MENU } from '@/constants/menu';
 import AirtightChart from './_components/AirtightChart';
@@ -19,40 +19,42 @@ export default function DetailPage({
 }) {
     return (
         <div className={styles.container}>
-            {avg && id ? (
-                <>
-                    <h3 className={styles.title}>
-                        당신의 <b>{MENU[id]?.title ?? '---'}</b>
-                        {MENU[id].href === 'airtight' ? '은' : '는'}&nbsp;&nbsp;
-                        <br />
-                        <span className={styles.emphasize}>
-                            <strong>{a && !Number.isNaN(a) ? Number(a).toFixed(1) : avg}</strong>
-                            {MENU[id].href === 'lighting' && (
-                                <span>
-                                    &nbsp;W/m<sup>2</sup>
-                                </span>
-                            )}
-                            {MENU[id].href === 'airtight' && (
-                                <span>
-                                    &nbsp;h<sup>-1</sup>
-                                </span>
-                            )}
-                        </span>
-                        &nbsp;&nbsp;입니다.
-                    </h3>
-                    {MENU[id].href === 'temperature' && <TemperatureChart />}
-                    {MENU[id].href === 'lighting' && <LightingChart />}
-                    {MENU[id].href === 'airtight' && 
-                        <AirtightChart 
-                            atype={atype ? Number(atype) : undefined}
-                            year={year ? Number(year) : undefined}
-                            avg={a ? Number(a) : avg ? Number(avg) : undefined}
-                        />
-                    }
-                </>
-            ) : (
-                '옵션 선택 후 진단하기 버튼을 눌러주세요.'
-            )}
+            <Suspense fallback={null}>
+                {avg && id ? (
+                    <>
+                        <h3 className={styles.title}>
+                            당신의 <b>{MENU[id]?.title ?? '---'}</b>
+                            {MENU[id].href === 'airtight' ? '은' : '는'}&nbsp;&nbsp;
+                            <br />
+                            <span className={styles.emphasize}>
+                                <strong>{a && !Number.isNaN(a) ? Number(a).toFixed(1) : avg}</strong>
+                                {MENU[id].href === 'lighting' && (
+                                    <span>
+                                        &nbsp;W/m<sup>2</sup>
+                                    </span>
+                                )}
+                                {MENU[id].href === 'airtight' && (
+                                    <span>
+                                        &nbsp;h<sup>-1</sup>
+                                    </span>
+                                )}
+                            </span>
+                            &nbsp;&nbsp;입니다.
+                        </h3>
+                        {MENU[id].href === 'temperature' && <TemperatureChart />}
+                        {MENU[id].href === 'lighting' && <LightingChart />}
+                        {MENU[id].href === 'airtight' && 
+                            <AirtightChart 
+                                atype={atype ? Number(atype) : undefined}
+                                year={year ? Number(year) : undefined}
+                                avg={a ? Number(a) : avg ? Number(avg) : undefined}
+                            />
+                        }
+                    </>
+                ) : (
+                    '옵션 선택 후 진단하기 버튼을 눌러주세요.'
+                )}
+            </Suspense>
         </div>
     );
 }
